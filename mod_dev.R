ec50161f4f91f38f4522d4d0033202a59bea6a9b
Oliver Eaton <eat_fred@protonmail.com>
2023-10-27 22:56:12 +1100

    progress

--- mod_dev.R
+++ mod_dev.R
@@ -6,51 +6,33 @@ library(baguette)
 library(doParallel)
 
 
-df_raw <- dh_getQuery(dh_createCon("postgres"), "SELECT * FROM nba.player_game_log WHERE season_type != 'All Star'") |> 
-  mutate(
-    next_pts = lead(pts, order_by = game_date), 
-    # team = str_sub(matchup, 1, 3),
-    # opponent = str_sub(matchup, str_locate(matchup, " @ | vs. ")[, "end"]),
-    # next_opponent = lead(opponent, order_by = game_date),
-    season_type = as.numeric(ordered(season_type, levels = c("Pre Season", "Regular Season", "Playoffs"))),
-    across(c(where(is.numeric), -next_pts), \(x) replace_na(x, 0)),
-    .by = player_id
-  ) |> 
-  filter(!is.na(next_pts))
 
+# Data --------------------------------------------------------------------
 
-
-# Split Data --------------------------------------------------------------
+df_raw <- dh_getQuery(dh_createCon("postgres"), "train.sql")
 
 df_split <- initial_split(df_raw)
-df_train <- training(df_split)
+df_train <- training(df_split) |> sample_n(9000)
 df_test <- testing(df_split)
 
 
 # Recipe ------------------------------------------------------------------
 
-rec <- recipe(data = df_train, next_pts ~ .) |> 
-  update_role(player_id, game_id, new_role = "id") |> 
-  step_rm(year_season, slug_season, video_available, matchup, game_date, wl) |> 
+rec_original <- recipe(data = df_train, pts ~ .) |> 
+  update_role(player_id, game_id, game_date, new_role = "id") |> 
   step_naomit(everything()) |> 
-  # probably should have a log step here too
-  step_range(c(all_numeric_predictors(), -season_type)) |> 
-  step_dummy(all_factor_predictors()) 
+  step_log(c(all_numeric_predictors(), -c(weight_kg, height_cm, avg_min, avg_plus_minus, ends_with("pct"))), offset = 1) |> 
+  step_range(all_numeric_predictors()) |> 
+  step_pca(all_numeric_predictors()) |> 
+  step_dummy(all_factor_predictors())
   
-  
-# view(x <- bake(prep(rec), new_data = NULL))
-recs <- mget(str_subset(objects(), "rec$"))
+recs <- mget(str_subset(objects(), "^rec_"))
+
+# x <- bake(prep(rec_original), new_data = NULL)
+# map(recs, \(x) count(bake(prep(x), new_data = NULL), over_20_pts))
 
 
 # Model Definitions -------------------------------------------------------
-
-# TAKE TOO LONG TO PROCESS 
-# boost_tree_xgboost_spec <- boost_tree(tree_depth = 30, trees = 500, learn_rate = tune(), min_n = 30, loss_reduction = tune(), sample_size = tune(), stop_iter = 5) |>
-#   set_engine("xgboost") |>
-#   set_mode("regression")
-# 
-# cubist_rules_Cubist_spec <- cubist_rules(committees = tune(), neighbors = tune(), max_rules = tune()) |>
-#   set_engine("Cubist")
 
 bag_mars_earth_spec <- bag_mars() |>
   set_engine("earth") |>
@@ -59,7 +41,7 @@ bag_mars_earth_spec <- bag_mars() |>
 linear_reg_glm_spec <- linear_reg() |>
   set_engine("glm")
 
-decision_tree_rpart_spec <- decision_tree(tree_depth = tune(), min_n = 30, cost_complexity = tune()) |>
+decision_tree_rpart_spec <- decision_tree(tree_depth = tune(), min_n = tune(), cost_complexity = tune()) |>
   set_engine("rpart") |>
   set_mode("regression")
 
@@ -73,34 +55,34 @@ mods <- mget(str_subset(objects(), "_spec$"))
 # Workflow ----------------------------------------------------------------
 
 # Register parallel backend
-# registerDoParallel(cores = parallel::detectCores(logical = FALSE))
-# 
-# # Tune models
-# cv_folds <- vfold_cv(df_train, v = 5)
-# wflows <- workflow_set(recs, mods) |>
-#   workflow_map(
-#     "tune_grid",
-#     resamples = cv_folds,
-#     grid = 10,
-#     verbose = TRUE,
-#     metrics = metric_set(mae, rmse, rsq),
-#     control = control_grid(parallel_over = "everything", verbose = TRUE)
-#   )
-# 
-# # Drop parallel (ie, re-register sequential processing)
-# registerDoSEQ()
-# save.image()
+registerDoParallel(cores = parallel::detectCores(logical = FALSE) / 2)
+
+# Tune models
+cv_folds <- vfold_cv(df_train, v = 5)
+wflows <- workflow_set(recs, mods) |>
+  workflow_map(
+    "tune_grid",
+    resamples = cv_folds,
+    grid = 10,
+    verbose = TRUE,
+    metrics = metric_set(mae, rmse, rsq),
+    control = control_grid(parallel_over = "everything", verbose = TRUE)
+  )
+
+# Drop parallel (ie, re-register sequential processing)
+registerDoSEQ()
+save.image()
 
 
 # Model Comparison --------------------------------------------------------
 
 autoplot(wflows)
-rank_results(wflows)
+view(rank_results(wflows))
 
 
 # Fit best model ----------------------------------------------------------
 
-mod_type <- "rec_rand_forest_ranger_spec"
+mod_type <- "rec_original_bag_mars_earth_spec"
 best_wflow <- extract_workflow(wflows, mod_type)
 
 best_mod <- wflows |> 
@@ -116,19 +98,30 @@ best_fit <- finalize_workflow(best_wflow, best_mod) |>
 df_test <- df_test |> 
   mutate(
     preds = predict(best_fit, df_test)[[1]],
-    pred_error = next_pts - preds
+    pred_error = pts - preds,
+    pred_bins = cut(preds, breaks = c(-10, 0, 10, 20, 30, 40))
   )
-
 
 # Results -----------------------------------------------------------------
 
-mae(df_test, next_pts, preds)
+mae(df_test, pts, preds)
 hist(df_test$pred_error)
 
-pivot_longer(df_test, c(next_pts, preds), names_to = "dist") |> 
-  ggplot(aes(x = value, fill = dist)) +
-  geom_density(alpha = 0.3) +
-  geom_vline(xintercept = 20)
+# Model is underpredicting when players score large, and
+# forecasted to be in the 20-30 bin
+df_test |> 
+  group_by(pred_bins) |> 
+  summarise(
+    bin_mae = sum(abs(preds - pts)) / n(),
+    bin_diff = sum(pts - preds)
+  )
+
+# Players performing well past the 20 point threshold
+filter(df_test, pred_bins == "(20,30]") |> 
+  ggplot(aes(x = pts)) +
+  geom_histogram(fill = "dodgerblue", colour = "black") +
+  geom_vline(xintercept = 20, colour = "red") +
+  theme_bw()
 
 
 # Extract model -----------------------------------------------------------
---
+++ queries/train.sql
@@ -0,0 +1,102 @@
+
+WITH cte_season_player_team AS (
+    SELECT *
+    FROM (
+        SELECT 
+            slug_season, 
+            player_id, 
+            team_slug,
+            CASE 
+                WHEN position = 'F-C' THEN 'C-F'
+                WHEN position = 'G-F' THEN 'F-G'
+                ELSE position
+            END AS position, 
+            weight_kg, 
+            height_cm, 
+            age, 
+            ROW_NUMBER() OVER(PARTITION BY slug_season, player_id ORDER BY how_acquired DESC) AS rn
+        FROM nba.team_roster
+    ) AS latest_team
+    WHERE rn = 1
+),
+
+cte_rolling_calcs AS (
+    SELECT 
+        log.game_date,
+        log.game_id,
+        log.player_id,
+        REPLACE(REGEXP_REPLACE(log.matchup, ' vs. | @ ', '', 'gi'), cte_season_player_team.team_slug, '') AS opponent, 
+        CASE WHEN log.matchup LIKE '%@%' THEN 'away' ELSE 'home' END AS home_away,
+        SUM(CASE WHEN log.wl = 'W' THEN 1 ELSE 0 END) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS win_count,
+        SUM(CASE WHEN log.wl = 'L' THEN 1 ELSE 0 END) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS lose_count,
+        AVG(log.min) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_min,
+        SUM(log.fgm) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS fgm,
+        AVG(log.fgm) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_fgm,
+        SUM(log.fga) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS fga,
+        AVG(log.fga) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_fga,
+        SUM(log.fg3_m) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS fg3m,
+        AVG(log.fg3_m) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_fg3m,
+        SUM(log.fg3_a) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS fg3a,
+        AVG(log.fg3_a) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_fg3a,
+        SUM(log.ftm) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS ftm,
+        AVG(log.ftm) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_ftm,
+        SUM(log.fta) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS fta,
+        AVG(log.fta) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_fta,
+        AVG(log.oreb) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_oreb,
+        AVG(log.dreb) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_dreb,
+        AVG(log.reb) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_reb,
+        AVG(log.ast) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_ast,
+        AVG(log.stl) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_stl,
+        AVG(log.blk) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_blk,
+        AVG(log.pf) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_pf,
+        AVG(log.plus_minus) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_plus_minus, 
+        AVG(log.pts) OVER(PARTITION BY log.player_id ORDER BY log.game_date ROWS BETWEEN 15 PRECEDING AND 1 PRECEDING) AS avg_pts, 
+        cte_season_player_team.team_slug, 
+        cte_season_player_team.position, 
+        cte_season_player_team.weight_kg, 
+        cte_season_player_team.height_cm, 
+        cte_season_player_team.age,
+        log.pts
+    FROM nba.player_game_log AS log
+    INNER JOIN cte_season_player_team ON log.player_id = cte_season_player_team.player_id
+        AND log.slug_season = cte_season_player_team.slug_season
+    WHERE log.slug_season < '2023-24'
+)
+
+SELECT 
+    game_date,
+    game_id,
+    player_id,
+--    team_slug,
+    position,
+    weight_kg,
+    height_cm,
+    age,
+--    opponent,
+    home_away,
+    win_count,
+    lose_count,
+    avg_min,
+    avg_fgm,
+    avg_fga,
+    CASE WHEN fga = 0 THEN 0 ELSE fgm / fga END AS fg_pct,
+    avg_fg3m,
+    avg_fg3a,
+    CASE WHEN fg3a = 0 THEN 0 ELSE fg3m / fg3a END AS fg3_pct,
+    avg_ftm,
+    avg_fta,
+    CASE WHEN fta = 0 THEN 0 ELSE ftm / fta END AS ft_pct,
+    avg_oreb,
+    avg_dreb,
+    avg_reb,
+    avg_ast,
+    avg_stl,
+    avg_blk,
+    avg_pf,
+    avg_plus_minus,
+    avg_pts,
+    pts
+FROM cte_rolling_calcs
+WHERE avg_pts IS NOT NULL
+
+ORDER BY player_id, game_date
